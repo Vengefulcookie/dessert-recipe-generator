0fb4b23e0b2dc3f6e6c6372d67c5ca85602009cb
Vengefulcookie <snethemba.nosipho.shangase@gmail.com>
2024-12-16 00:51:51 +0200

    Edited some css in the style.css file, added an animation for when the recipe is still generating

--- javascript/index.js
+++ javascript/index.js
@@ -13,12 +13,14 @@ function requestRecipe(event) {
   let userInstructions = document.querySelector("#user-instructions");
   let apiKey = "a9e857fe88f94odb09ad1fcdt90348f2";
   let context =
-    "You are a AI assistant that is well versed in different types of delicious desserts and their recipes.Please provide the recipe with no added extra content in HTML format but do not state that it is in html. Make the title be in a <strong> element and it must be smaller than the heading in size but bigger than the recipe";
+    "You are a AI assistant that is well versed in different types of delicious desserts and their recipes.Please provide the recipe with no added extra content in HTML format but do not state that it is in html. Please behave";
   let prompt = `Generate a dessert recipe for ${userInstructions.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  axios.get(apiUrl).then(provideRecipe);
 
-  alert("Your guide to a sweet treat is on its way!😋");
+  let recipeIncoming = document.querySelector("#recipe");
+  recipeIncoming.innerHTML = ` <div class = "waiting">Your guide to a sweet treat is on its way!😋</div>`;
+
+  axios.get(apiUrl).then(provideRecipe);
 }
 
 let recipeForm = document.querySelector("#recipe-form");
